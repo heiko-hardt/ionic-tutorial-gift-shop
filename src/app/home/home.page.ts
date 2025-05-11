f548e2a2c053979911ad24e99613b7ca51e08e46
Heiko Hardt <heiko@hardt.dev>
2025-05-11 18:31:02 +0200

    [TASK] Inject api service and provide items

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,20 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+  items: any[] = [];
+  allItems: any[] = [];
+
+  private api = inject(ApiService);
+
   constructor() {}
+
+  ngOnInit() {
+    console.log('ngOnInit ()');
+    this.getItem();
+  }
+
+  getItem() {
+    this.allItems = this.api.items;
+    this.items = [...this.allItems];
+  }
 }
