be5cdcba04a8beaed23886f6a757383d8aa259f3
Heiko Hardt <heiko@hardt.dev>
2025-05-11 22:45:41 +0200

    [TASK] Fetch and provide item

--- src/app/home/item-detail/item-detail.page.ts
+++ src/app/home/item-detail/item-detail.page.ts
@@ -1,20 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+} from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular/standalone';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.page.html',
   styleUrls: ['./item-detail.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class ItemDetailPage implements OnInit {
-
-  constructor() { }
+  id!: string;
+  item: any;
+  private route = inject(ActivatedRoute);
+  private navCtrl = inject(NavController);
+  private api = inject(ApiService);
+  constructor() {}
 
   ngOnInit() {
+    this.getItem();
   }
 
+  getItem() {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log('check id: %o', id);
+    if (!id || id == '0') {
+      this.navCtrl.back();
+      return;
+    }
+    this.id = id;
+    this.item = this.api.items.find((record) => record.id == id);
+    console.log('this.item: %o', this.item);
+  }
 }
