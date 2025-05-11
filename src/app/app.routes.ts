481aef793304d7935a7d7f818f9573d2024fb550
Heiko Hardt <heiko@hardt.dev>
2025-05-11 22:05:17 +0200

    [TASK] Create detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'item-detail',
+    loadComponent: () => import('./home/item-detail/item-detail.page').then( m => m.ItemDetailPage)
+  },
 ];
